a7713e68b9514a20b35a3c4fd57d8d52c2258a15
jasmineolmos <54619537+jasmineolmos@users.noreply.github.com>
2019-11-14 20:22:01 -1000

    update assignment1

--- Olmos_Jasmine_Assignment1/public/product_list.js
+++ Olmos_Jasmine_Assignment1/public/product_list.js
@@ -2,21 +2,24 @@ var products = [
 {  
     "product":"Lipstick",  
     "price": 10.00,  
-    "image": "./lipstick.jpg" 
+    "image": "./lipstick.jpg", 
+    "description": "A bright red lipstick made to turn heads."
 },
 {
     "product":"Eyeshadow",  
     "price": 20.00,  
-    "image": "./eyeshadow.jpg" 
+    "image": "./eyeshadow.jpg",
+    "description": "A light pink eyeshadow to wear in the day, can also be used as blush." 
 },
 {
     "product":"Foundation",  
     "price": 15.00,  
-    "image": "./foundation.jpg" 
+    "image": "./foundation.jpg",
+    "description": "Foundation with SPF to protect you from the sun when you're out and about."
 }
 
 ];
-// referenced from lab 13
+// referenced from lab 13, exports the product data so we can get it in the product_display
 if(typeof module != 'undefined') {
     module.exports.products = products;
   }
--- Olmos_Jasmine_Assignment1/server.js
+++ Olmos_Jasmine_Assignment1/server.js
@@ -1,24 +1,24 @@
-var data = require('./public/product_list.js'); //get the data from product_list.js
-var products = data.products;
+var data = require('./public/product_list.js'); //get the data from product_list.js
+var products = data.products;
 
-var express = require('express');
-var app = express();
-var myParser = require("body-parser");
-var data = require("./public/product_list.js");
+var express = require('express');
+var app = express();
+var myParser = require("body-parser");
 
-app.all('*', function (request, response, next) {
-    console.log(request.method + ' to ' + request.path)
-    next();
+
+app.all('*', function (request, response, next) {
+    console.log(request.method + ' to ' + request.path)
+    next();
 });
 
-app.use(myParser.urlencoded({ extended: true }));
-//to process the response from what is typed in the form
-app.post("/process_form", function (request, response) {
-   let POST = request.body;
-   if (typeof POST['quantity_textbox'] != 'undefined') {
-    displayPurchase(POST, response);
-    
-} 
+app.use(myParser.urlencoded({ extended: true }));
+//to process the response from what is typed in the form
+app.post("/product_form", function (request, response) {
+   let POST = request.body;
+   if (typeof POST['quantity_textbox'] != 'undefined') {
+    displayPurchase(POST, response);
+    
+} 
 });
 app.use(express.static('./public'));
-app.listen(8080, () => console.log(`listening on port 8080`));
+app.listen(8080, () => console.log(`listening on port 8080`)); //starting the server listening on port 8080
